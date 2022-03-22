96ebfb8a5ce771b388c23eb4200f497cbf8f82fc
junui <wnsspdl@naver.com>
2022-03-22 18:37:58 +0900

    infinitescroll API 통신 완료

--- infiniteScroll/index.js
+++ infiniteScroll/index.js
@@ -1,10 +1,46 @@
 (()=> {
-  const get = (target) => { document.querySelector(target) }
+  const get = (target) => document.querySelector(target)
+  const $posts = get('.posts')
+  
+  const showPosts = (posts) => {
+    posts.forEach(post => {
+      const $post = document.createElement('div')
+      $post.classList.add('post')
+      $post.innerHTML = `
+          <div class="header">
+            <div class="id">${post.id}.</div>
+            <div class="title">${post.title}</div>
+          </div>
+          <div class="body">${post.body}</div>
+      `
+      $posts.appendChild($post)
+    })
+  }
+
+  const loadPosts = async () => {
+    try {
+      const response = await fetchPosts()
+      showPosts(response)
+    } catch(error) {
+      console.error(error)
+    }
+  }
+
+  const fetchPosts = async () => {
+    const API_URL = `https://jsonplaceholder.typicode.com/posts`
+    const response = await fetch(API_URL)
+    if (!response.ok) {
+      throw Error('error')
+    }
+    return await response.json()
+  }
   const onScroll = () => {
+
     
   }
   
-  window.addEventListener('DOMContentLoaded', () => {
-    loadPosts()
+  window.addEventListener('DOMContentLoaded', async () => {
+    const posts = await loadPosts()
+    
   })
 })();
