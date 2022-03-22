f370e4f15fa6c3dcb0b16093468dcd0be8c2d9d2
junui <wnsspdl@naver.com>
2022-03-22 18:47:08 +0900

    infinitescroll 구현 완료

--- infiniteScroll/index.js
+++ infiniteScroll/index.js
@@ -1,7 +1,20 @@
 (()=> {
   const get = (target) => document.querySelector(target)
   const $posts = get('.posts')
-  
+  const $loader = get('.loader')
+  const limit = 10
+  let total = 10
+  let currentPage = 1
+  let end = 100
+
+  const hideLoader = () => {
+    $loader.classList.remove('show')
+  }
+
+  const showLoader = () => {
+    $loader.classList.add('show')
+  }
+
   const showPosts = (posts) => {
     posts.forEach(post => {
       const $post = document.createElement('div')
@@ -17,30 +30,44 @@
     })
   }
 
-  const loadPosts = async () => {
+  const loadPosts = async (currentPage, limit) => {
+    showLoader()
     try {
-      const response = await fetchPosts()
+      const response = await fetchPosts(currentPage, limit)
       showPosts(response)
     } catch(error) {
       console.error(error)
+    } finally {
+      hideLoader()
     }
   }
 
-  const fetchPosts = async () => {
-    const API_URL = `https://jsonplaceholder.typicode.com/posts`
+  const fetchPosts = async (currentPage, limit) => {
+    const API_URL = `https://jsonplaceholder.typicode.com/posts?_page=${currentPage}&_limit=${limit}`
     const response = await fetch(API_URL)
     if (!response.ok) {
       throw Error('error')
     }
     return await response.json()
   }
-  const onScroll = () => {
 
-    
+  const handleScroll = () => {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement    
+    if (total === end) {
+      window.removeEventListener('scroll', handleScroll)
+      return
+    }
+
+    if (scrollTop + clientHeight >= scrollHeight - 5) {
+      currentPage++
+      total += 10
+      loadPosts(currentPage, limit)
+      return
+    }
   }
   
-  window.addEventListener('DOMContentLoaded', async () => {
-    const posts = await loadPosts()
-    
+  window.addEventListener('DOMContentLoaded', () => {
+    loadPosts(currentPage,limit)
+    window.addEventListener('scroll', handleScroll)
   })
 })();
