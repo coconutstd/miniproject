adcd2ee88bba4cd9ade75837ed7b48e58dd0726f
junui <wnsspdl@naver.com>
2022-03-22 18:29:02 +0900

    infinitescroll 시작

---
+++ infiniteScroll/index.js
@@ -0,0 +1,10 @@
+(()=> {
+  const get = (target) => { document.querySelector(target) }
+  const onScroll = () => {
+    
+  }
+  
+  window.addEventListener('DOMContentLoaded', () => {
+    loadPosts()
+  })
+})();
