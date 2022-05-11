8a200d4af15a302a67839ae882a6dda5d9eabe09
coconutstd <wnsspdl@naver.com>
2022-05-11 15:08:29 +0900

    React연동을 위한 백엔드 구현 완료

--- 리액트관련/blog/blog-backend/src/api/posts/index.js
+++ 리액트관련/blog/blog-backend/src/api/posts/index.js
@@ -1,15 +1,17 @@
 import Router from "koa-router"
 import * as postsCtrl from "./posts.ctrl"
+import checkLoggedIn from "../../lib/checkLoggedIn"
 
 const posts = new Router()
+
 posts.get("/", postsCtrl.list)
-posts.post("/", postsCtrl.write)
+posts.post("/", checkLoggedIn, postsCtrl.write)
 
 const post = new Router()
-post.get("/:id", postsCtrl.read)
-post.delete("/:id", postsCtrl.remove)
-post.patch("/:id", postsCtrl.update)
+post.get("/", postsCtrl.read)
+post.delete("/", checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.remove)
+post.patch("/", checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.update)
 
-posts.use("/:id", postsCtrl.checkObjectId, post.routes())
+posts.use("/:id", postsCtrl.getPostById, post.routes())
 
 export default posts
--- 리액트관련/blog/blog-backend/src/main.js
+++ 리액트관련/blog/blog-backend/src/main.js
@@ -5,7 +5,7 @@ import bodyParser from "koa-bodyparser"
 import mongoose from "mongoose"
 
 import api from "./api"
-import createFakeData from "./createFakeData"
+import jwtMiddleware from "./lib/jwtMiddleware"
 
 const { PORT, MONGO_URI } = process.env
 console.log(PORT, MONGO_URI)
@@ -14,7 +14,6 @@ mongoose
   .connect(MONGO_URI, { useFindAndModify: false })
   .then(() => {
     console.log("Connected to MongoDB")
-    createFakeData()
   })
   .catch((e) => {
     console.error(e)
@@ -26,6 +25,7 @@ const router = new Router()
 router.use("/api", api.routes())
 
 app.use(bodyParser())
+app.use(jwtMiddleware)
 
 app.use(router.routes()).use(router.allowedMethods())
 
