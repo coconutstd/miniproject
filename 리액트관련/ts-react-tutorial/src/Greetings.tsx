5f74645dcd502fd35d7536269fd87bbcc2614b53
junui <wnsspdl@naver.com>
2022-05-14 16:41:35 +0900

    리액트 타입스크립트 컴포넌트 만들기 연습

---
+++ 리액트관련/ts-react-tutorial/src/Greetings.tsx
@@ -0,0 +1,26 @@
+import React from "react"
+
+type GreetingsProps = {
+  name: string
+  mark: string
+  optional?: string
+  onClick: (name: string) => void
+}
+
+function Greetings({ name, mark, optional, onClick }: GreetingsProps) {
+  return (
+    <div>
+      Hello, {name} {mark}
+      {optional && <p>{optional}</p>}
+      <div>
+        <button onClick={() => onClick(name)}>Click Me</button>
+      </div>
+    </div>
+  )
+}
+
+Greetings.defaultProps = {
+  mark: "!",
+}
+
+export default Greetings
